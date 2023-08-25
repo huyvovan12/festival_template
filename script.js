a21c5c885f3edfc947b170a54c19159e4ac65855
MACBOOK <macbook@MACBOOKs-MacBook-Pro-2.local>
2023-08-25 09:35:05 +0700

    done page content

--- script.js
+++ script.js
@@ -1,98 +1,63 @@
-const contentDiv = document.getElementById("content");
-const navLinks = document.querySelectorAll("nav a");
 
-async function loadPage(pageId) {
-    try {
-        const response = await fetch(`${pageId}/index.html`);
-        const pageContent = await response.text();
-        contentDiv.innerHTML = pageContent;
+const hoverDiv = document.querySelector('.home__header');
 
-        //script homepage
-        if (window.location.hash === '#home' || window.location.hash === '') {
-            const hoverDiv = document.querySelector('.home__header');
+hoverDiv?.addEventListener('mouseover', () => {
+    hoverDiv.classList.add('hover');
+});
 
-            hoverDiv?.addEventListener('mouseover', () => {
-                hoverDiv.classList.add('hover');
-            });
+const container = document.getElementById("table__list");
+const loadMoreButton = document.getElementById("load-more");
+const homeTableContainer = document.getElementById("home__film_table");
 
-            const container = document.getElementById("table__list");
-            const loadMoreButton = document.getElementById("load-more");
-            const homeTableContainer = document.getElementById("home__film_table");
+const item = '<div class="table__item"><div class="table__item--img"><img src="../img/image_bg.png" alt="" /></div><div class="table__item--header">Column heading here</div><div class="table__item--author">By Jordan Roberts, Octobers</div><div class="table__item--hashtag"><span class="item">hash tag</span><span class="item">hash tag</span><span class="item">hash tag</span></div></div>'
 
-            const item = '<div class="table__item"><div class="table__item--img"><img src="../img/image_bg.png" alt="" /></div><div class="table__item--header">Column heading here</div><div class="table__item--author">By Jordan Roberts, Octobers</div><div class="table__item--hashtag"><span class="item">hash tag</span><span class="item">hash tag</span><span class="item">hash tag</span></div></div>'
+const items = [
+    item, item, item, item, item, item, item, item, item
+];
 
-            const items = [
-                item, item, item, item, item, item, item, item, item
-            ];
+let visibleItemCount = 4; // Số phần tử hiển thị ban đầu
+let isShortened = false; // Biến cờ để theo dõi trạng thái
 
-            let visibleItemCount = 4; // Số phần tử hiển thị ban đầu
-            let isShortened = false; // Biến cờ để theo dõi trạng thái
-
-            function renderItems(startIndex, endIndex) {
-                for (let i = startIndex; i < endIndex; i++) {
-                    const itemDiv = document.createElement("div");
-                    itemDiv.innerHTML = items[i];
-                    container.appendChild(itemDiv);
-                }
-            }
-
-            function toggleVisibility() {
-                visibleItemCount = isShortened ? 4 : items.length;
-                isShortened = !isShortened;
-                container.innerHTML = "";
-                loadMoreButton.textContent = isShortened ? "Load More" : "Shorten";
-                homeTableContainer.classList.toggle("show-items", isShortened);
-
-                setTimeout(() => {
-                    renderItems(0, visibleItemCount);
-                    const targetElement = document.getElementById('load-more');
-                    if (targetElement) {
-                        const yOffset = window.pageYOffset + targetElement.getBoundingClientRect().top;
-                        window.scrollTo({ top: yOffset / 2, behavior: "smooth" });
-                    }
-                }, isShortened ? 100 : 140);
-
-            }
-
-            loadMoreButton.addEventListener("click", toggleVisibility);
-
-
-            renderItems(0, visibleItemCount);
-
-
-            const topWhite = document.getElementById('home__bg-top');
-            const bottomWhite = document.getElementById('home__bg-bottom');
-
-            console.log(document.getElementById('home__film_table').clientWidth);
-
-
-            topWhite.style.width = `${document.getElementById('home__film_table').clientWidth}px`;
-            bottomWhite.style.width = `${document.getElementById('home__film_table').clientWidth}px`;
-            console.log(topWhite.style.width)
-        }
-
-        //script homepage
-
-    } catch (error) {
-        console.error("Error loading page:", error);
+function renderItems(startIndex, endIndex) {
+    for (let i = startIndex; i < endIndex; i++) {
+        const itemDiv = document.createElement("div");
+        itemDiv.innerHTML = items[i];
+        container.appendChild(itemDiv);
     }
 }
 
-function navigate(event) {
-    event.preventDefault();
-    const pageId = event.target?.getAttribute("href").substr(1);
-    loadPage(pageId);
+function toggleVisibility() {
+    visibleItemCount = isShortened ? 4 : items.length;
+    isShortened = !isShortened;
+    container.innerHTML = "";
+    loadMoreButton.textContent = isShortened ? "Load More" : "Shorten";
+    homeTableContainer.classList.toggle("show-items", isShortened);
+
+    setTimeout(() => {
+        renderItems(0, visibleItemCount);
+        const targetElement = document.getElementById('load-more');
+        if (targetElement) {
+            const yOffset = window.pageYOffset + targetElement.getBoundingClientRect().top;
+            window.scrollTo({ top: yOffset / 2, behavior: "smooth" });
+        }
+    }, isShortened ? 100 : 140);
+
 }
 
-navLinks.forEach(link => {
-    link.addEventListener("click", navigate);
-});
+loadMoreButton.addEventListener("click", toggleVisibility);
 
-// Load initial page
-loadPage("home");
 
-window.addEventListener('hashchange', navigate);
-window.addEventListener('load', navigate);
+renderItems(0, visibleItemCount);
+
+
+const topWhite = document.getElementById('home__bg-top');
+const bottomWhite = document.getElementById('home__bg-bottom');
+
+console.log(document.getElementById('home__film_table').clientWidth);
+
+
+topWhite.style.width = `${document.getElementById('home__film_table').clientWidth}px`;
+bottomWhite.style.width = `${document.getElementById('home__film_table').clientWidth}px`;
 
 let rotationAngle = 0;
 
